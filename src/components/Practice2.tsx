9fbc2f1ec4b73425dcdc3df090b93ca3a68883cb
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-01-31 17:27:58 +0300

    Create Practice2.tsx

---
+++ src/components/Practice2.tsx
@@ -0,0 +1,49 @@
+const Practice2 = () => {
+  return (
+    <>
+      <main className="">
+        <header className="">
+          <h1 className="">Compund selectors are useful</h1>
+        </header>
+        <section className="section-one">
+          <h2 className="">Section-One Title</h2>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.{" "}
+            <a href="" className="">
+              Deleniti obcaecati
+            </a>{" "}
+            aspernatur nulla voluptate sequi. Aut deserunt vero, animi quae
+            laboriosam quidem maiores nesciunt pariatur iusto odit amet debitis.
+            Magnam, temporibus.
+          </p>
+          <a href="" className="btn">
+            Contact me
+          </a>
+          <a href="" className="btn">
+            Learn more
+          </a>
+        </section>
+
+        <section className="section-two">
+          <h2 className="">Section-Two Title</h2>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A velit{" "}
+            <a href="" className="">
+              perferendis officiis
+            </a>{" "}
+            reprehenderit ducimus soluta beatae ex odio? Officiis quo inventore
+            numquam ex dignissimos provident cumque iure. Neque, suscipit et!
+          </p>
+          <a href="" className="btn">
+            Contact me
+          </a>
+          <a href="" className="btn">
+            Learn more
+          </a>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Practice2;
