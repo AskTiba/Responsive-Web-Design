3ee91520ebf29c98365eadd1c15a4c06c08fc626
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-03 06:14:49 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import Exercise2 from "./components/Exercise2";
 // import Exercise1 from "./components/Exercise1";
 // import Practice1 from "./components/Practice1";
-import Practice2 from "./components/Practice2";
+// import Practice2 from "./components/Practice2";
 
 function App() {
   return (
     <>
       {/* <Practice1 /> */}
       {/* <Exercise1/> */}
-      <Practice2 />
+      {/* <Practice2 /> */}
+      <Exercise2/>
     </>
   );
 }
