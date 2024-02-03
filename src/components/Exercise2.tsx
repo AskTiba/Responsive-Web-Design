c533e0c5781f5d6beef699228c29e54fab634477
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-03 06:14:40 +0300

    Create Exercise2.tsx

---
+++ src/components/Exercise2.tsx
@@ -0,0 +1,51 @@
+import img from "../assets/boardwalk.png";
+
+const Exercise2 = () => {
+  return (
+    <>
+      <main className="w-[620px] mx-auto">
+        <header className="bg-[#145858]">
+          <h1 className="text-white">CSS is a lot of fun</h1>
+        </header>
+        <section className="text-[#555]">
+          <h2 className="font-[700]">Section One Title</h2>
+          <img src={img} alt="" className="mt-5" />
+          <p className="mt-5">
+            Lorem ipsum dolor sit amet{" "}
+            <a href="" className="">
+              consectetur adipisicing
+            </a>{" "}
+            elit. In perferendis dolorem maxime, alias tempora facere.
+          </p>
+        </section>
+        <section className="bg-[#145858] text-white">
+          <h2 className="font-[700]">Section Two Title</h2>
+          <p className="">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+            qui maiores quisquam explicabo nesciunt harum laudantium aperiam
+            sapiente error, quas, temporibus sit modi, ad dignissimos.
+          </p>
+          <p className="">
+            Lorem{" "}
+            <a href="" className="">
+              ipsum dolor
+            </a>{" "}
+            sit amet consectetur adipisicing elit. Sapiente, doloribus. Tempora
+            ratione sed fugit modi molestias veritatis consectetur assumenda,
+            quibusdam nulla ad iure maxime alias.
+          </p>
+          <div className="flex gap-10">
+            <button className="button bg-[#f7b318] text-white hover:bg-green-600 hover:text-black ">
+              Contact us
+            </button>
+            <button className="button hover:bg-blue-600 hover:text-white ">
+              More info
+            </button>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Exercise2;
