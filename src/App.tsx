a99d929e168dc32ca84e2e9566d5cd7b23eaa801
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-03-08 13:14:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import "./App.css";
-import Exercise2 from "./components/Exercise2";
-// import Exercise1 from "./components/Exercise1";
-// import Practice1 from "./components/Practice1";
-// import Practice2 from "./components/Practice2";
-
-function App() {
+const App = () => {
   return (
-    <>
-      {/* <Practice1 /> */}
-      {/* <Exercise1/> */}
-      {/* <Practice2 /> */}
-      <Exercise2/>
-    </>
+    <header className="bg-[#f8f8f8] py-[2em]">
+      <div className="container w-[92%] mx-auto flex items-center justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-[2rem] font-[Lora] font-[400] text-[#143774]">
+            Living the simple life.
+          </h1>
+          <p className="text-[#1792d2] text-[.75rem] -mt-[.5em]">
+            A blog exploring minimalism in life.
+          </p>
+        </div>
+        <nav className="">
+          <ul className="flex gap-[1em] justify-center">
+            <li className="">
+              <a href="#" className="underline">
+                Home
+              </a>
+            </li>
+            <li className="">
+              <a href="#">About Me</a>
+            </li>
+            <li className="">
+              <a href="#">Recent Posts</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
-}
+};
 
 export default App;
